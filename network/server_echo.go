8ed1e044f24ac051d9e86a140894e7bc1f7dbfe1
cgil <lao.tseu.is.alive@gmail.com>
2019-10-04 16:41:19 +0200

    tcp echo server

---
+++ network/server_echo.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+)
+
+func main() {
+
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+	for {
+		fmt.Println("Waiting for client...")
+		conn, err := l.Accept()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Connection from %v\n", conn.RemoteAddr())
+
+		msg, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			fmt.Printf("received empty string, conn:  %v\n", conn)
+			_, err = io.WriteString(conn, "An empty string is not a good way to talk together, don't you think so ? ")
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			fmt.Printf("Will send back this received msg:  %v\n", msg)
+			_, err = io.WriteString(conn, "Received: "+string(msg))
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+		conn.Close()
+	}
+}
